68d871812971029bab9211069c7248358042ea3c
George Garside <grg.garside@gmail.com>
2018-06-21 23:47:49 +0100

    Stack Exchange resources folder

---
+++ o/se/ad.js
@@ -0,0 +1,1 @@
+document.write('<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9733787518596382" data-ad-slot="4777202833" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>')
---
+++ o/se/old/adfooter.js
@@ -0,0 +1,1 @@
+document.write('<ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-9733787518596382" data-ad-slot="6537128838"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>')
